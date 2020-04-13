afbe31a2097ae9a806811ab1a04a689902be9eda
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-13 17:48:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,29 @@
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 require 'pry'
 
 class Song
-  attr_accessor :name
-  attr_reader :artist
-
+  extend Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+  
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
+    
   def self.all
     @@songs
   end
+  
+  attr_accessor :name
+  attr_reader :artist
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
+  def initialize
+    @songs << self
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
